0c4333b4a043c705c1d82c7deb9e512112b94642
Anastasiya-Zhoroven <anastasiya.zhoroven@gmail.com>
2024-03-22 11:17:32 +0100

    refactored with eslint and added styles for form

--- src/app/form/email.validator.ts
+++ src/app/form/email.validator.ts
@@ -1,30 +1,26 @@
-import { AbstractControl, FormControl, ValidatorFn } from '@angular/forms';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
-
-export function emailValidator(): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any } | null => {
-        if (!control.value) {
-            return null; // Возвращаем null, если значение пустое
-        }
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        const valid = emailRegex.test(control.value);
-        return valid ? null : { invalidEmail: true };
+export function emailValidator (): ValidatorFn {
+  return (control: AbstractControl): Record<string, any> | null => {
+    if (!control.value) {
+      return null;
     }
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const valid = emailRegex.test(control.value);
+    return valid ? null : { invalidEmail: true };
+  };
 }
 
-export function ipv4Validator(): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any } | null => {
-      if (!control.value) {
-        return null; // Возвращаем null, если значение пустое
-      }
-  
-      // Паттерн для проверки IPv4 адреса
-      const ipv4Pattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-  
-      // Проверяем соответствие значения паттерну
-      const isValid = ipv4Pattern.test(control.value);
-  
-      // Возвращаем результат валидации
-      return isValid ? null : { 'invalidIpv4': true };
-    };
-  }
+export function ipv4Validator (): ValidatorFn {
+  return (control: AbstractControl): Record<string, any> | null => {
+    if (!control.value) {
+      return null;
+    }
+
+    const ipv4Pattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+    const isValid = ipv4Pattern.test(control.value);
+
+    return isValid ? null : { invalidIpv4: true };
+  };
+}
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { emailValidator, ipv4Validator } from './email.validator';
-
 
 @Component({
   selector: 'app-form',
@@ -13,15 +12,15 @@ export class FormComponent {
   isEmailControlFocused: boolean = false;
   isIPv4ControlFocused: boolean = false;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor (private readonly formBuilder: FormBuilder) {
     this.emailForm = this.formBuilder.group({
-      'email': new FormControl('', [Validators.required, emailValidator()]),
-      'ipv4': new FormControl('', [Validators.required, ipv4Validator()])
+      email: new FormControl('', [Validators.required, emailValidator()]),
+      ipv4: new FormControl('', [Validators.required, ipv4Validator()])
     });
   }
-  
-  onSubmit() {
-    this.emailForm.markAllAsTouched()
+
+  onSubmit (): void {
+    this.emailForm.markAllAsTouched();
     if (this.emailForm.valid) {
       console.log('Form submitted successfully!');
     } else {
@@ -29,27 +28,27 @@ export class FormComponent {
     }
   }
 
-  handleEmailControlFocus() {
+  handleEmailControlFocus (): void {
     this.isEmailControlFocused = true;
   }
 
-  handleEmailControlBlur() {
+  handleEmailControlBlur (): void {
     this.isEmailControlFocused = false;
   }
 
-  handleIPv4ControlFocus() {
+  handleIPv4ControlFocus (): void {
     this.isIPv4ControlFocused = true;
   }
 
-  handleIPv4ControlBlur() {
+  handleIPv4ControlBlur (): void {
     this.isIPv4ControlFocused = false;
   }
 
-  get email() {
+  get email (): AbstractControl<any, any> | null {
     return this.emailForm.get('email');
   }
 
-  get ipv4() {
+  get ipv4 (): AbstractControl<any, any> | null {
     return this.emailForm.get('ipv4');
   }
 }
